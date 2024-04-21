35eed2da68fb7204a4f53965b8ec218afb7da8b5
swpowe <spencer.powell@me.com>
2024-04-20 18:57:31 -0600

    Finished Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        string firstName, lastName;
+
+        Console.Write("What is your first name? ");
+        firstName = Console.ReadLine();
+        
+        Console.Write("What is your last name? ");
+        lastName = Console.ReadLine();
+
+        firstName = char.ToUpper(firstName[0]) + firstName.Substring(1).ToLower();
+        lastName = char.ToUpper(lastName[0]) + lastName.Substring(1).ToLower();
+
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
