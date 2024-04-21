87e440a8ccb8146ab4e0e857cdfba36c0ebe0ed6
swpowe <spencer.powell@me.com>
2024-04-20 19:47:40 -0600

    Finished Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        char gradeLetter;
+        int gradeNum;
+        decimal gradePercent;
+        bool passed;
+        
+        // Ask the user for grade (as percentage)
+        Console.Write("What is your grade as a percent? ");
+        gradePercent = Decimal.Parse(Console.ReadLine());
+        gradeNum = (int)Decimal.Round(gradePercent, 0);
+
+        // Print the letter grade
+        if (gradeNum >= 90) {
+            gradeLetter = 'A';
+            passed = true;
+        }else if (gradeNum >= 80) {
+            gradeLetter = 'B';
+            passed = true;
+        }else if (gradeNum >= 70) {
+            gradeLetter = 'C';
+            passed = true;
+        }else if (gradeNum >= 60) {
+            gradeLetter = 'D';
+            passed = false;
+        }else {
+            gradeLetter = 'F';
+            passed = false;
+        }
+
+        Console.WriteLine($"Your current letter grade is {gradeLetter}.");
+
+        //Display passed if > 70 or failed < 70
+        if(passed) {
+            Console.WriteLine("Congratulations! You're passing the class! Great Job.");
+        }else {
+            Console.WriteLine("Unfortunately it looks like you're currently failing. Keep trying!");
+        }
     }
 }
