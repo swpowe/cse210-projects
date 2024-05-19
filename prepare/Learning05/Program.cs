2dfb394bddb6bce56e604d3953049bcff5d61375
swpowe <spencer.powell@me.com>
2024-05-18 22:59:13 -0600

    Learning 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new();
+        
+        Square square = new Square("blue", 4);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("yellow", 4, 3);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("purple", 5);
+        shapes.Add(circle);
+
+        foreach (var shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
+
     }
 }
